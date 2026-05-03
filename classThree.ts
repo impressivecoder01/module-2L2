c3641db8cabe51bca009ce926981a44223846ea8
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-04 02:42:19 +0600

    generic

--- classThree.ts
+++ classThree.ts
@@ -7,7 +7,7 @@ const rollNumber: Array<number> = [2,2,1,34,45]
 const isEligibleList: Array<boolean> = [true,false,true]
 
 type GenericArray = Array<string>
-type GenericArrayF<value> = Array<value>
+type GenericArrayF<T> = Array<T>
 const friendsF: GenericArrayF<string> = ['mrx','mry','mrz']
 const rollNumberF: GenericArrayF<number> = [2,2,1,34,45]
 const isEligibleListF: GenericArrayF<boolean> = [true,false,true]
@@ -15,3 +15,16 @@ const isEligibleListF: GenericArrayF<boolean> = [true,false,true]
 // const sqrFun = (value: number):number => {
 //    return value*value //not important
 // }
+type Coordinates<X,Y> = [X, Y]
+const coordinates:Coordinates<number, number> = [21,32]
+const coordinates1:Coordinates<string,string> = ['21','32']
+const userList = [
+    {
+        nam: 'mx.x',
+        age: 66
+    },
+    {
+        nam: 'mr.y',
+        age: 33
+    }
+]
