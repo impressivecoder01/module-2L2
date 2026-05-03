e95dfab7cdd695658e506f06a2c6ab25c2683994
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-04 00:55:16 +0600

    classONe

--- classOne.ts
+++ classOne.ts
@@ -2,4 +2,19 @@ let anything: any;
 // anything = 33;
 // (anything as number)
 anything = 'md';
-(anything as string)
+// (anything as string)
+const kgToGmConvert = (input: string | number): string | number |undefined => {
+    if (typeof input === 'number'){
+        return input*1000
+    }
+    else if(typeof input === 'string'){
+        const [value] = input.split(' ')
+        console.log(value)
+        return `converted output is: ${ Number(value)*1000}`
+    }
+
+}
+const result = kgToGmConvert(2) as number
+console.log(result);
+const result2 = kgToGmConvert('2 tk') as string
+console.log(result2);
