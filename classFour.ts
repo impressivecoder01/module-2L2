40e78479842acd6d87128966494c88fc837e3a02
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-05 00:16:29 +0600

    classForu

--- classFour.ts
+++ classFour.ts
@@ -1,26 +1,51 @@
-interface Developer<T>{
-    name: string,
-    salary: number,
-    device: {
-        brand: string,
-        model: string,
-        releaseYear: string
-    },
-    smartWatch: T
+interface Developer<T> {
+  name: string;
+  salary: number;
+  device: {
+    brand: string;
+    model: string;
+    releaseYear: string;
+  };
+  smartWatch: T;
 }
-const poorDeveloper :Developer<{
-    heartRate: string,
-    stopWatch: boolean
-}> = {
-    name: 'Mr. Poor',
-    salary: 20,
-    device: {
-        brand: 'lenovo',
-        model: 'A21',
-        releaseYear: '2000'
-    },
-    smartWatch: {
-        heartRate: '200',
-        stopWatch: true
-    } 
+
+interface BrandCharaWatch  {
+heartRate: string;
+  stopWatch: boolean;
 }
+const poorDeveloper: Developer<BrandCharaWatch> = {
+  name: "Mr. Poor",
+  salary: 20,
+  device: {
+    brand: "lenovo",
+    model: "A21",
+    releaseYear: "2000",
+  },
+  smartWatch: {
+    heartRate: "200",
+    stopWatch: true,
+  },
+};
+interface ApppleWatch {
+ heartRate: string;
+  callSupport: boolean;
+  calculator: boolean;
+  AI: boolean;
+}
+const richDeveloper: Developer<ApppleWatch> = {
+  name: "Mr. Rich",
+  salary: 20555,
+  device: {
+    brand: "hp",
+    model: "A421",
+    releaseYear: "2600",
+  },
+  smartWatch: {
+    heartRate: "200",
+    callSupport: true,
+    calculator: true,
+    AI: true,
+  },
+};
+
+console.log(richDeveloper)
