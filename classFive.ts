13ff9486d111d01bf74681adf0b55b153a1f5080
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-05 00:50:38 +0600

    class five done

--- classFive.ts
+++ classFive.ts
@@ -9,3 +9,29 @@ const createArrayWithGeneric = <T>(value:T)=>{
 const arrString = createArrayWithGeneric('apple')
 const arrNumber = createArrayWithGeneric(33)
 const arrObj = createArrayWithGeneric({id: 3, name:'mr'})
+
+//tuple 
+const createArrayWithoutTuple = (params: string, params2: number) => [params,params2]
+const createArrayWithTuple = <X,Y>(params: X, params2 :Y) => [params,params2]
+const res1 = createArrayWithTuple('mr', 333)
+console.log(res1);
+const res2 = createArrayWithTuple(333, false)
+console.log(res2)
+
+const addStudentToCourse = <T>(studentInfo: T) => {
+    return {course: 'PH', ...studentInfo}
+}
+const student1 ={
+    id: 343,
+    name: 'mahabub',
+    hasPen: true
+}
+const student2 = {
+    id: 3245,
+    hasCar: 'big',
+    hasWife: false
+}
+const result = addStudentToCourse(student1)
+console.log(result)
+const result2= addStudentToCourse(student2)
+console.log(result2)
